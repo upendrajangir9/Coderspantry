cc8fef355db05884ceacecd2923488ca86f57b36
UD <upendrajangir9@gmail.com>
2018-04-06 22:13:14 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\UD\PycharmProjects\Coder's Pantry\master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
